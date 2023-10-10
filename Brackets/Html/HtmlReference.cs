40f566450bd350af4248f988221b56f1d8d683a3
Vladislav Borovikov <vborovikov@live.ru>
2023-10-10 14:27:38 +0500

    added common attributes

--- Brackets/Html/HtmlReference.cs
+++ Brackets/Html/HtmlReference.cs
@@ -4,6 +4,9 @@ public class HtmlReference : Document.MarkupReference<HtmlLexer>
 {
     public HtmlReference()
     {
+        // tags
+        //
+
         // void elements
         AddReference(new TagReference("!doctype", this) { IsParent = false });
         AddReference(new TagReference("area", this) { IsParent = false });
@@ -73,5 +76,133 @@ public class HtmlReference : Document.MarkupReference<HtmlLexer>
         AddReference(new TagReference("tt", this) { Level = ElementLevel.Inline });
         AddReference(new TagReference("var", this) { Level = ElementLevel.Inline });
         AddReference(new TagReference("video", this) { Level = ElementLevel.Inline });
+
+        // attributes
+        //
+
+        AddReference(new AttributeReference("accept", this));
+        AddReference(new AttributeReference("accept-charset", this));
+        AddReference(new AttributeReference("accesskey", this));
+        AddReference(new AttributeReference("action", this));
+        AddReference(new AttributeReference("align", this));
+        AddReference(new AttributeReference("allow", this));
+        AddReference(new AttributeReference("alt", this));
+        AddReference(new AttributeReference("async", this));
+        AddReference(new AttributeReference("autocapitalize", this));
+        AddReference(new AttributeReference("autocomplete", this));
+        AddReference(new AttributeReference("autoplay", this));
+        AddReference(new AttributeReference("background", this));
+        AddReference(new AttributeReference("bgcolor", this));
+        AddReference(new AttributeReference("border", this));
+        AddReference(new AttributeReference("buffered", this));
+        AddReference(new AttributeReference("capture", this));
+        AddReference(new AttributeReference("charset", this));
+        AddReference(new AttributeReference("checked", this));
+        AddReference(new AttributeReference("cite", this));
+        AddReference(new AttributeReference("class", this));
+        AddReference(new AttributeReference("color", this));
+        AddReference(new AttributeReference("cols", this));
+        AddReference(new AttributeReference("colspan", this));
+        AddReference(new AttributeReference("content", this));
+        AddReference(new AttributeReference("contenteditable", this));
+        AddReference(new AttributeReference("contextmenu", this));
+        AddReference(new AttributeReference("controls", this));
+        AddReference(new AttributeReference("coords", this));
+        AddReference(new AttributeReference("crossorigin", this));
+        AddReference(new AttributeReference("csp", this));
+        AddReference(new AttributeReference("data", this));
+        AddReference(new AttributeReference("datetime", this));
+        AddReference(new AttributeReference("decoding", this));
+        AddReference(new AttributeReference("default", this));
+        AddReference(new AttributeReference("defer", this));
+        AddReference(new AttributeReference("dir", this));
+        AddReference(new AttributeReference("dirname", this));
+        AddReference(new AttributeReference("disabled", this));
+        AddReference(new AttributeReference("download", this));
+        AddReference(new AttributeReference("draggable", this));
+        AddReference(new AttributeReference("enctype", this));
+        AddReference(new AttributeReference("enterkeyhint", this));
+        AddReference(new AttributeReference("for", this));
+        AddReference(new AttributeReference("form", this));
+        AddReference(new AttributeReference("formaction", this));
+        AddReference(new AttributeReference("formenctype", this));
+        AddReference(new AttributeReference("formmethod", this));
+        AddReference(new AttributeReference("formnovalidate", this));
+        AddReference(new AttributeReference("formtarget", this));
+        AddReference(new AttributeReference("headers", this));
+        AddReference(new AttributeReference("height", this));
+        AddReference(new AttributeReference("hidden", this));
+        AddReference(new AttributeReference("high", this));
+        AddReference(new AttributeReference("href", this));
+        AddReference(new AttributeReference("hreflang", this));
+        AddReference(new AttributeReference("http-equiv", this));
+        AddReference(new AttributeReference("id", this));
+        AddReference(new AttributeReference("integrity", this));
+        AddReference(new AttributeReference("intrinsicsize", this));
+        AddReference(new AttributeReference("inputmode", this));
+        AddReference(new AttributeReference("ismap", this));
+        AddReference(new AttributeReference("itemprop", this));
+        AddReference(new AttributeReference("kind", this));
+        AddReference(new AttributeReference("label", this));
+        AddReference(new AttributeReference("lang", this));
+        AddReference(new AttributeReference("language", this));
+        AddReference(new AttributeReference("loading", this));
+        AddReference(new AttributeReference("list", this));
+        AddReference(new AttributeReference("loop", this));
+        AddReference(new AttributeReference("low", this));
+        AddReference(new AttributeReference("manifest", this));
+        AddReference(new AttributeReference("max", this));
+        AddReference(new AttributeReference("maxlength", this));
+        AddReference(new AttributeReference("minlength", this));
+        AddReference(new AttributeReference("media", this));
+        AddReference(new AttributeReference("method", this));
+        AddReference(new AttributeReference("min", this));
+        AddReference(new AttributeReference("multiple", this));
+        AddReference(new AttributeReference("muted", this));
+        AddReference(new AttributeReference("name", this));
+        AddReference(new AttributeReference("novalidate", this));
+        AddReference(new AttributeReference("open", this));
+        AddReference(new AttributeReference("optimum", this));
+        AddReference(new AttributeReference("pattern", this));
+        AddReference(new AttributeReference("ping", this));
+        AddReference(new AttributeReference("placeholder", this));
+        AddReference(new AttributeReference("playsinline", this));
+        AddReference(new AttributeReference("poster", this));
+        AddReference(new AttributeReference("preload", this));
+        AddReference(new AttributeReference("readonly", this));
+        AddReference(new AttributeReference("referrerpolicy", this));
+        AddReference(new AttributeReference("rel", this));
+        AddReference(new AttributeReference("required", this));
+        AddReference(new AttributeReference("reversed", this));
+        AddReference(new AttributeReference("role", this));
+        AddReference(new AttributeReference("rows", this));
+        AddReference(new AttributeReference("rowspan", this));
+        AddReference(new AttributeReference("sandbox", this));
+        AddReference(new AttributeReference("scope", this));
+        AddReference(new AttributeReference("scoped", this));
+        AddReference(new AttributeReference("selected", this));
+        AddReference(new AttributeReference("shape", this));
+        AddReference(new AttributeReference("size", this));
+        AddReference(new AttributeReference("sizes", this));
+        AddReference(new AttributeReference("slot", this));
+        AddReference(new AttributeReference("span", this));
+        AddReference(new AttributeReference("spellcheck", this));
+        AddReference(new AttributeReference("src", this));
+        AddReference(new AttributeReference("srcdoc", this));
+        AddReference(new AttributeReference("srclang", this));
+        AddReference(new AttributeReference("srcset", this));
+        AddReference(new AttributeReference("start", this));
+        AddReference(new AttributeReference("step", this));
+        AddReference(new AttributeReference("style", this));
+        AddReference(new AttributeReference("summary", this));
+        AddReference(new AttributeReference("tabindex", this));
+        AddReference(new AttributeReference("target", this));
+        AddReference(new AttributeReference("title", this));
+        AddReference(new AttributeReference("translate", this));
+        AddReference(new AttributeReference("type", this));
+        AddReference(new AttributeReference("usemap", this));
+        AddReference(new AttributeReference("value", this));
+        AddReference(new AttributeReference("width", this));
+        AddReference(new AttributeReference("wrap", this));
     }
 }
