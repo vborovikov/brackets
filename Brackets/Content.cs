3040b94857d0fe4ff09849b1b65b3c3bacf182bf
Vladislav Borovikov <vborovikov@live.ru>
2023-10-11 16:47:29 +0500

    added base class CharacterData

--- Brackets/Content.cs
+++ Brackets/Content.cs
@@ -2,6 +2,27 @@
 {
     using System;
 
+    public abstract class CharacterData : Element
+    {
+        protected CharacterData(int start)
+            : base(start) { }
+
+        public override string ToString()
+        {
+            return this.Source[this.Start..this.End].ToString();
+        }
+
+        internal override string ToDebugString()
+        {
+            return String.Concat(this.Source[this.Start..].TrimStart()[..Math.Min(15, this.Length)], "\u2026");
+        }
+
+        public bool Contains(ReadOnlySpan<char> text)
+        {
+            return this.Source[this.Start..this.End].Contains(text, StringComparison.CurrentCultureIgnoreCase);
+        }
+    }
+
     public class Content : Element
     {
         private int end;
@@ -40,17 +61,20 @@
         }
     }
 
-    public class Section : Content
+    public class Section : CharacterData
     {
         private readonly int dataStart;
         private readonly int dataLength;
 
         public Section(int start, int length, int dataStart, int dataLength)
-            : base(start, length)
+            : base(start)
         {
+            this.End = start + length;
             this.dataStart = dataStart;
             this.dataLength = dataLength;
         }
+
+        public override int End { get; }
 
         public ReadOnlySpan<char> Data => this.Source.Slice(this.dataStart, this.dataLength);
 
@@ -58,17 +82,15 @@
         {
             return this.Data.ToString();
         }
-
-        internal override bool TryAdd(Content content) => false;
     }
 
-    public sealed class Comment : Content
+    public sealed class Comment : CharacterData
     {
-        public Comment(int start, int length)
-            : base(start, length)
+        public Comment(int start, int length) : base(start)
         {
+            this.End = start + length;
         }
 
-        internal override bool TryAdd(Content content) => false;
+        public override int End { get; }
     }
 }
